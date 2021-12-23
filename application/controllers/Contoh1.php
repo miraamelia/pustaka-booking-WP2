c6dfca9a58d605a625f5e56df8ba12131461152b
miraamelia <miraamelia041@gmail.com>
2021-12-23 23:46:40 +0700

    first commit

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,8 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "Selamat Datang, Selamat Belajar Web Programming";
+    }
+}
---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Mira</li>
+        <li>Nama Belakang : Amelia</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jalan Tanjung Pura, Jakarta Barat</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Jakarta</li>
+    </ul>
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Bulutangkis</li>
+        <li>Futsal</li>
+    </ul>
+</section>
